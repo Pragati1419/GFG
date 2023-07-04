ba8a9c8615cf8542e7d8f347fcd4caf26ad20620
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2023-07-04 09:55:53 +0530

    Updated Solution - GfG to GitHub

--- Medium/Subset Sums/subset-sums.cpp
+++ Medium/Subset Sums/subset-sums.cpp
@@ -6,19 +6,19 @@ using namespace std;
 class Solution
 {
 public:
-void fun(int ind , int sum ,vector<int> &arr, int N  ,vector<int>&v){
+void fun(int ind , int sum , vector<int>&arr,int N,vector<int>&v){
     if(ind==N){
         v.push_back(sum);
         return;
     }
-    fun(ind+1,sum+arr[ind], arr ,N,v);
-    fun(ind+1 , sum ,arr,N ,v);
+    fun(ind+1,sum+arr[ind],arr,N,v);
+    fun(ind+1,sum,arr,N,v);
 }
     vector<int> subsetSums(vector<int> arr, int N)
     {
         // Write Your Code here
         vector<int>v;
-        fun(0 , 0 ,arr , N ,v);
+        fun(0,0,arr,N,v);
         sort(v.begin(),v.end());
         return v;
     }
