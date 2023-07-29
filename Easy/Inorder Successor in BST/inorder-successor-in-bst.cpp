dcc24ca191046784609e3d637448b5ffbfb8cb44
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2023-07-30 03:11:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Inorder Successor in BST/inorder-successor-in-bst.cpp
@@ -0,0 +1,179 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+Node* search(Node* root, int key)
+{
+    // Base Cases: root is null or key is present at root
+    if (root == NULL || root->data == key)
+       return root;
+    
+    // Key is greater than root's key
+    if (root->data < key)
+       return search(root->right, key);
+ 
+    // Key is smaller than root's key
+    return search(root->left, key);
+}
+
+
+// } Driver Code Ends
+/*The structure of Node
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+  void inorder(Node *root,vector<int>&v){
+      if(root==NULL)
+      return;
+      inorder(root->left,v);
+      v.push_back(root->data);
+      inorder(root->right,v);
+  }
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+    Node * inOrderSuccessor(Node *root, Node *x)
+    {
+        //Your code here
+        Node* successor = NULL;
+         Node* curr = root;
+    while (curr != nullptr && curr != x) {
+        if (curr->data > x->data) {
+            // Update successor whenever we move to the left child
+            successor = curr;
+            curr = curr->left;
+        } else {
+            curr = curr->right;
+        }
+    }
+    if(curr==NULL)
+    return NULL;
+    if (curr->right != nullptr) {
+        // In case of right subtree, the successor will be the leftmost node of the right subtree
+        curr = curr->right;
+        while (curr->left != nullptr) {
+            curr = curr->left;
+        }
+        return curr;
+    } else {
+        // If the node x does not have a right subtree, return the updated successor
+        return successor;
+    }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* head = buildTree(s);
+        
+        int k;
+        cin>>k;
+        getchar();
+        
+        Node* kNode = search( head, k );
+        Solution obj;
+        Node* suc = obj.inOrderSuccessor(head, kNode);
+        
+        if( suc == NULL ) cout<< "-1";
+        else cout<< suc->data;
+        cout << endl;
+    }
+    
+    return 1;
+}
+// } Driver Code Ends
